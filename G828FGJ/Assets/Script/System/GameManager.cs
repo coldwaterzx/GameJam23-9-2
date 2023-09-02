80720e598fcead1ec77bf047d0e8943c76933641
小仔 <71455438+chris58530@users.noreply.github.com>
2023-09-02 12:03:30 +0800

    gamemanager

--- G828FGJ/Assets/Script/System/GameManager.cs
+++ G828FGJ/Assets/Script/System/GameManager.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMove : MonoBehaviour
+public class GameManager : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -14,11 +14,5 @@ public class PlayerMove : MonoBehaviour
     void Update()
     {
         
-    }
-    void Move(){
-        float Horizontal = Input.GetAxis("Horizontal");
-        float Vetical = Input.GetAxis("Vertical");
-
-        Vector2 move = new Vector2(Horizontal,Vetical)*Time.deltaTime;
     }
 }
